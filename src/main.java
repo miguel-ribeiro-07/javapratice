bfe167a1e864fda4b3ee60cbdc2e387bc8d04c1a
Miguel Ribeiro <miguel.ribeiro.07@protonmail.com>
2024-07-31 23:47:08 -0300

    1 day, pratice git

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import pack2.testando;
+
+public class main {
+    public static void main(String[] args){
+        hello();
+        testando.hello2();
+    }
+
+    public static void hello(){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/pack2/testando.java
@@ -0,0 +1,7 @@
+package pack2;
+
+public class testando {
+    public static void hello2(){
+        System.out.println("Hello World 2!");
+    }
+}
